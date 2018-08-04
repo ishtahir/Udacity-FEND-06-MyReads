dfc3ff3941f38bf462954e68c98c9b59b412705d
Ish Tahir <ishtahir@icloud.com>
2018-08-04 17:48:14 -0500

    Fixed import statements on search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import * as BooksAPI from '.././BooksAPI';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
 
 class Search extends Component {
     render() {
@@ -14,7 +14,8 @@ class Search extends Component {
                             You can find these search terms here:
                             https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if you don't find a specific author or title. Every search is limited by search terms.
+                            However, remember that the BooksAPI.search method DOES search by title or author.
+                            So, don't worry if you don't find a specific author or title. Every search is limited by search terms.
                         */}
                         <input type="text" placeholder="Search by title or author"/>
                     </div>

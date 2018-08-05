37b976815b0618bff76efa25c50fad3b047309d2
Ish Tahir <ishtahir@icloud.com>
2018-08-05 17:08:40 -0500

    Cleaned up all the code in Bookshelf component

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import Book from './Book.jsx';
 
 class Bookshelf extends Component {
     render() {
         return (
-
+            <div className="list-books-content">
+                <div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Currently Reading</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                <Book />
+                            </ol>
+                        </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Want to Read</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                <Book />
+                            </ol>
+                        </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                <Book />
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }

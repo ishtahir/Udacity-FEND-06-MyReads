b89c8c82b13edd5d51f9921b6581179fa16f1d72
Ish Tahir <ishtahir@icloud.com>
2018-08-07 20:42:47 -0500

    Added renderShelf method now all books are appearing in their right shelves

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class Book extends Component {
     render() {
-        console.log('books.props', this.props);
+        // console.log('Book.props', this.props);
         const imgUrl = this.props.books.imageLinks ? `url(${this.props.books.imageLinks.thumbnail})` : 'url("")';
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: {imgUrl} }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `${imgUrl}` }}></div>
                     <div className="book-shelf-changer">
                         <select>
                             <option value="move" disabled>Move to...</option>
@@ -19,7 +19,7 @@ class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{this.props.books.title}</div>
-                <div className="book-authors">{this.props.books.authors}</div>
+                <div className="book-authors">{this.props.books.authors[0]}</div>
             </div>
         )
     }
--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -6,38 +6,53 @@ class Bookshelf extends Component {
     state = {
         currentlyReading: [],
         wantToRead: [],
-        read: [],
-        titles: ['Currently Reading', 'Want to Read', 'Read']
+        read: []
+    }
+
+    componentDidMount() {
+        this.getBooks();
     }
 
     getBooks() {
-        BooksAPI.getAll().then(book => {
-            book.shelf === 'currentlyReading' ? this.setState(this.state.currentlyReading.push(book)) : null;
-            book.shelf === 'wantToRead' ? this.setState(this.state.wantToRead.push(book)) : null;
-            book.shelf === 'read' ? this.setState(this.state.read.push(book)) : null;
-        })
+        BooksAPI.getAll().then(books => {
+            books.forEach(book => {
+                book.shelf === 'currentlyReading' ? this.state.currentlyReading.push(book) : null;
+                book.shelf === 'wantToRead' ? this.state.wantToRead.push(book) : null;
+                book.shelf === 'read' ? this.state.read.push(book) : null;
+            })
+        });
+        console.log('currently', this.state.currentlyReading);
+        console.log('want to', this.state.wantToRead);
+        console.log('read', this.state.read);
     }
 
     changeShelf(book, shelf) {
         BooksAPI.update(book, shelf).then(() => this.getbooks());
     }
 
+    renderShelf(title, books) {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map((book, index) => {
+                            return (<Book books={book} key={index} change={this.changeShelf} />)
+                        })}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+
     render() {
-        // const titles = ['Currently Reading', 'Want to Read', 'Read'];
         return (
             <div className="list-books-content">
-                {this.state.titles.map((title, index) => {
-                    return (
-                        <div className="bookshelf" key={index}>
-                            <h2 className="bookshelf-title">{title}</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-
-                                </ol>
-                            </div>
-                        </div>
-                    )
-                })}
+                <div>
+                    {this.renderShelf('Currently Reading', this.state.currentlyReading)}
+                    {this.renderShelf('Want to Read', this.state.wantToRead)}
+                    {this.renderShelf('Read', this.state.read)}
+                </div>
             </div>
         )
     }

1d5926712fb67ab6cd32011e3a1d08032fa44ee6
Ish Tahir <ishtahir@icloud.com>
2018-08-09 19:25:17 -0500

    Search seems to be working fully

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -23,7 +23,6 @@ class Search extends Component {
     }
 
     render() {
-        console.log('books', this.props.books)
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -37,7 +36,7 @@ class Search extends Component {
                             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                             you don't find a specific author or title. Every search is limited by search terms.
                         */}
-                        <input type="text" placeholder="Search by title or author" onChange={event => this.handleSearch(event.target.value.trim())} value={this.state.query}/>
+                        <input type="text" placeholder="Search by title or author" onChange={event => this.handleSearch(event.target.value)} value={this.state.query}/>
                     </div>
                 </div>
                 <div className="search-books-results">
